5642e73b8af0233f6622332ab122987c636d3eeb
Abner Saavedra <ingenieroabnersaavedra@bitbucket.org>
2023-05-25 19:11:31 -0400

    Commit inicial: avances fundamentales

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Books from './routes/books';
+import Users from './routes/users';
+import { ReactKeycloakProvider } from "@react-keycloak/web";
+import keycloak from './config/Keycloak';
+import PrivateRoute from "./helpers/PrivateRoute";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
+  <ReactKeycloakProvider authClient={keycloak}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route path="routes/books" element={<Books />} />
+          <Route path="routes/users" element={<Users />} />
+        </Route>
+      </Routes>
+
+      {/* <React.StrictMode>
+        <App />
+      </React.StrictMode> */}
+    </BrowserRouter>
+  </ReactKeycloakProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/routes/books.js
@@ -0,0 +1,7 @@
+export default function Books() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Books</h2>
+      </main>
+    );
+}
